cd3df70f920e7a92274c88161d301f27bca10124
benjamin99 <cknomber1@gmail.com>
2016-04-28 17:36:02 +0800

    setup the index.js as the entry point of the package

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 var requirePostfixModifier = require('./gulp-require-postfix-modifier.js');
 var cryptor = require('./gulp-cryptor.js');
+
+module.exports = {
+    cryptor: cryptor,
+    requirePostfixModifier: requirePostfixModifier
+}
